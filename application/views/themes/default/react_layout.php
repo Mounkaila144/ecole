3e87311c92b79204ac5f5039ae4ff7aa47d135e6
Mounkaila <mounkaila144@gmail.com>
2025-11-06 20:21:41 +0100

    new

---
+++ application/views/themes/default/react_layout.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo isset($page['title']) ? $page['title'] : 'Elegant Higher Education Website'; ?></title>
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>react-app/logo.png" type="image/x-icon">
+    <style>
+        /* Reset minimal pour éviter les conflits */
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body {
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+                'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+                sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+    </style>
+</head>
+<body>
+    <?php echo $content; ?>
+</body>
+</html>
